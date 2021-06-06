ca87d72e8c3d5744acaff2a08c3a7b5c858c25a2
aqib.javed <aqibbutt3078@gmail.com>
2021-06-06 06:22:36 +0200

    basic oop, basics

---
+++ src/main/scala/TwoSum.scala
@@ -0,0 +1,22 @@
+package com.example.scala
+
+import java.util.Arrays
+
+object TwoSum extends App {
+
+  println(Arrays.toString(twoSum(Array(3, 3), 6)))
+
+  def twoSum(nums: Array[Int], target: Int): Array[Int] = {
+    var indexByNumber: Map[Int, Int] = Map()
+    val result = new Array[Int](2)
+    nums.zipWithIndex.foreach{case (item, index) =>
+      val diff = target - item
+      if(indexByNumber.contains(diff) && index != indexByNumber.getOrElse(diff, -1)) {
+        return Array(indexByNumber.getOrElse(diff, -1), index)
+      } else
+      indexByNumber += (item -> index)
+    }
+
+    result
+  }
+}
